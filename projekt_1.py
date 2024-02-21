a11124927be20b2d4e8c3464227a24dfcdd0cd5a
Zuzana Burešová <zuza.buresova@centrum.cz>
2024-02-21 19:02:20 +0100

    Opravená verze

--- projekt_1.py
+++ projekt_1.py
@@ -8,54 +8,73 @@ discord: zuza_34473
 
 from task_template import TEXTS
 
-registrovani_uzivatele = {
+registred_users = {
     "bob" : "123", "ann" : "pass123", 
     "mike" : "password123", "liz" : "pass123"}
-oddelovac = "-" * 40
+separator = "-" * 40
+
+text_words = []
+titlecase = []
+uppercase = []
+lowercase = []
+numeric = []
+length_words = []
+lenghts_frequency = dict()
+
 # prihlaseni uzivatelu a kontrola jejich registrace
 user = (input("username:")).lower()
 password = input("password:")
 
-if user not in registrovani_uzivatele \
-    or password != registrovani_uzivatele[user]:
-    print("unregistered user, terminating the program..")
+if user not in registred_users \
+    or password != registred_users[user]:
+    print("Unregistered user, terminating the program..")
     quit()
-print(f"{oddelovac}\nWelcome to the app, {user}\n\
-We have 3 texts to be analyzed.\n{oddelovac}")
+print(f"{separator}\nWelcome to the app, {user}\n\
+We have 3 texts to be analyzed.\n{separator}")
 
 # analyza textu (number_text => TEXTS[number_text - 1])
-number_text = input("Enter a number btw. 1 and 3 to select: ")
-for pismeno in number_text:
-    if not pismeno.isdigit():
-        print("incorrect text number, terminating the program..")
+try:
+    number_text = int(input("Enter a number btw. 1 and 3 to select: "))
+    if number_text not in range(1, len(TEXTS)+1):
+        print("Incorrect text number, terminating the program..")
         quit()
-number_text = int(number_text)
-if number_text not in range(1,4):
-    print("incorrect text number, terminating the program..")
-    quit()
-print(oddelovac)
-text_slova = [slovo.strip(",.?! ") for slovo in TEXTS[number_text - 1].split()]
+except ValueError:
+        print("Incorrect input, enter a number, terminating the program..")
+        quit()
 
+print(separator)
 
-print("There are", len(text_slova), "words in the selected text.")
-titlecase = [(slovo.istitle() and slovo.isalpha()) for slovo in text_slova]
-print("There are", titlecase.count(True), "titlecase words.")
-uppercase = [(slovo.isupper() and slovo.isalpha()) for slovo in text_slova]
-print("There are", uppercase.count(True), "uppercase words.")
-lowercase = [(slovo.islower() and slovo.isalpha()) for slovo in text_slova]
-print("There are", lowercase.count(True), "lowercase words.")
-numeric = [slovo for slovo in text_slova if slovo.isnumeric()]
-print("There are", len(numeric), "numeric strings.")
-numeric_int = [int(slovo) for slovo in numeric]
-print("The sum of all the numbers", sum(numeric_int))
+for word in TEXTS[number_text - 1].split():
+    text_words.append(word.strip(",.?! "))
+
+for word in text_words:
+    if word.istitle() and word.isalpha():
+        titlecase.append(word)
+    elif word.isupper() and word.isalpha():
+        uppercase.append(word)
+    elif word.islower() and word.isalpha():
+        lowercase.append(word)
+    elif word.isnumeric():
+        numeric.append(int(word))
+    length_words.append(len(word))
+
+print(f"There are {len(text_words)} words in the selected text.")
+print(f"There are {len(titlecase)} titlecase words.")
+print(f"There are {len(uppercase)} uppercase words.")
+print(f"There are {len(lowercase)} lowercase words.")
+print(f"There are {len(numeric)} numeric strings.")
+print(f"The sum of all the numbers {sum(numeric)}")
 
 # cetnost ruznych delek slov v textu
-delky_slov = sorted([len(slovo) for slovo in text_slova])
-cetnost_delek = {delka: delky_slov.count(delka) for delka in delky_slov}
-max_cetnost = max(cetnost_delek.values())
+length_words = sorted(length_words)
 
-print(oddelovac)
-print("LEN", "OCUURENCES".center(max_cetnost+2), "NR.", sep = "|")
-print(oddelovac)
-for delka, cetnost in cetnost_delek.items():
-    print(str(delka).rjust(3), ("*" * cetnost).ljust(max_cetnost + 2), cetnost, sep = "|")
+for lenght in length_words:
+    lenghts_frequency.update({lenght: length_words.count(lenght)})
+max_frequency = max(lenghts_frequency.values())
+
+print(separator)
+print(f"LEN|{"OCCURENCES".center(max_frequency+2)}|NR.")
+print(separator)
+
+for lenght, frequency in lenghts_frequency.items():
+    print(f"{str(lenght).rjust(3)}|{("*" * frequency).ljust(max_frequency + 2)}|{frequency}")
